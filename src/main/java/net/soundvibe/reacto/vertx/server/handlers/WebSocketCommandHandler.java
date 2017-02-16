31e1968207f044b949aba414c1e7cfe2bf79c744
Linas Naginionis <linas.naginionis@storebrand.no>
2017-02-16 18:51:27 +0200

    working version

--- src/main/java/net/soundvibe/reacto/vertx/server/handlers/WebSocketCommandHandler.java
+++ src/main/java/net/soundvibe/reacto/vertx/server/handlers/WebSocketCommandHandler.java
@@ -5,8 +5,10 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.logging.*;
 import net.soundvibe.reacto.internal.InternalEvent;
-import net.soundvibe.reacto.server.*;
-import rx.Subscription;
+import net.soundvibe.reacto.mappers.Mappers;
+import net.soundvibe.reacto.server.CommandProcessor;
+import net.soundvibe.reacto.types.*;
+import rx.*;
 
 import java.util.Objects;
 
@@ -40,34 +42,45 @@ public class WebSocketCommandHandler implements Handler<ServerWebSocket> {
         serverWebSocket
             .setWriteQueueMaxSize(Integer.MAX_VALUE)
             .frameHandler(new WebSocketFrameHandler(buffer -> {
-                final Subscription subscription = commandProcessor.process(buffer.getBytes())
-                        .map(event -> internalEventToBytes(InternalEvent.onNext(event)))
+                final Subscription subscription = Observable.just(buffer.getBytes())
+                        .map(Mappers::fromBytesToCommand)
+                        .flatMap(command -> commandProcessor.process(command)
+                                .materialize()
+                                .doOnNext(eventNotification -> writeEventNotification(eventNotification, command, serverWebSocket))
+                                .dematerialize()
+                        )
                         .subscribe(
-                                bytes -> writeOnNext(bytes, serverWebSocket),
-                                error -> writeOnError(error, serverWebSocket),
-                                () -> writeOnCompleted(serverWebSocket)
+                                event -> log.info("Event was processed: " + event),
+                                error -> log.error("Error when mapping from notification: " + error),
+                                () -> log.info("Command successfully processed")
                         );
                 serverWebSocket
                         .exceptionHandler(exception -> {
                             log.error("ServerWebSocket exception: " + exception);
                             subscription.unsubscribe();
                         })
-                        .closeHandler(__ -> subscription.unsubscribe());
+                        .closeHandler(__ -> subscription.unsubscribe())
+                ;
             }));
     }
 
-    private void writeOnNext(byte[] bytes, ServerWebSocket serverWebSocket) {
+    private static void writeEventNotification(Notification<Event> eventNotification, Command command, ServerWebSocket serverWebSocket) {
+        switch (eventNotification.getKind()) {
+            case OnNext:
+                writeOnNext(internalEventToBytes(InternalEvent.onNext(eventNotification.getValue(), command.id.toString())), serverWebSocket);
+                break;
+            case OnError:
+                writeOnNext(internalEventToBytes(InternalEvent.onError(eventNotification.getThrowable(), command.id.toString())), serverWebSocket);
+                break;
+            case OnCompleted:
+                writeOnNext(internalEventToBytes(InternalEvent.onCompleted(command.id.toString())), serverWebSocket);
+                break;
+            default: throw new IllegalStateException("Unknown rx notification type: " + eventNotification);
+        }
+    }
+
+    private static void writeOnNext(byte[] bytes, ServerWebSocket serverWebSocket) {
         serverWebSocket.writeBinaryMessage(Buffer.buffer(bytes));
-    }
-
-    private void writeOnError(Throwable error, ServerWebSocket serverWebSocket) {
-        serverWebSocket.writeBinaryMessage(Buffer.buffer(internalEventToBytes(InternalEvent.onError(error))));
-        serverWebSocket.close();
-    }
-
-    private void writeOnCompleted(ServerWebSocket serverWebSocket) {
-        serverWebSocket.writeBinaryMessage(Buffer.buffer(internalEventToBytes(InternalEvent.onCompleted())));
-        serverWebSocket.close();
     }
 
     private boolean shouldHandle(String path) {
